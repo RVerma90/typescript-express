89b007a0a050d6bb0bc398be2921c88bed00c461
RVerma90 <irahulverma90@gmail.com>
2018-07-07 15:00:23 +0100

    initial commit

---
+++ app/server.ts
@@ -0,0 +1,24 @@
+/* app/server.ts */
+
+// Import everything from express and assign it to the express variable
+require('dotenv').config();
+
+import express from 'express';
+
+// Import SignUpController from controllers entry point
+import {SignUpController } from './controllers';
+
+// Create a new express application instance
+const app: express.Application = express();
+// The port the express app will listen on
+const port: number = 3000;
+
+// Mount the SignUpController at the /signp route
+app.use('/signup', SignUpController);
+
+// Serve the application at the given port
+app.listen(port, () => {
+    // Success callback
+    console.log(`Listening at http://localhost:${port}/`);
+    console.log(process.env.DB);
+});
---
+++ build/server.js
@@ -0,0 +1,23 @@
+"use strict";
+/* app/server.ts */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import everything from express and assign it to the express variable
+require('dotenv').config();
+var express_1 = __importDefault(require("express"));
+// Import SignUpController from controllers entry point
+var controllers_1 = require("./controllers");
+// Create a new express application instance
+var app = express_1.default();
+// The port the express app will listen on
+var port = 3000;
+// Mount the SignUpController at the /signp route
+app.use('/signup', controllers_1.SignUpController);
+// Serve the application at the given port
+app.listen(port, function () {
+    // Success callback
+    console.log("Listening at http://localhost:" + port + "/");
+    console.log(process.env.DB);
+});
